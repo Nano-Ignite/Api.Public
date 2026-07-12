3864f70a3ea92dd829cbfa51d3da57b5cf8637c1
vivet <vivet@vivetonline.com>
2026-07-12 14:14:48 +0200

    Updated

---
+++ Api.Public/Program.cs
@@ -0,0 +1,12 @@
+﻿using Nano.App.Api;
+using Nano.Logging.Extensions;
+using Nano.Logging.Serilog;
+
+NanoApiApplication
+    .ConfigureApp()
+    .ConfigureServices(x =>
+    {
+        x.AddNanoLogging<SerilogProvider>();
+    })
+    .Build()
+    .Run();
